dc9f03f312056ec5db07796928fff07c5479f1be
Tan <lenhattan2313@gmail.com>
2024-09-04 09:18:33 +0700

    Handle error api login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,11 +1,13 @@
 "use server";
 import { authActions } from "@/actions/auth/authActions";
 import { setCookies } from "@/lib/cookieUtils";
+import { HttpError } from "@/lib/error";
 import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { StatusCodes } from "http-status-codes";
 
 export async function POST(request: Request) {
-  const dataForm: LoginBodyType = await request.json();
   try {
+    const dataForm: LoginBodyType = await request.json();
     const response = await authActions.login(dataForm);
     const {
       data: { accessToken, refreshToken },
@@ -14,6 +16,16 @@ export async function POST(request: Request) {
     setCookies("refreshToken", refreshToken);
     return Response.json(response);
   } catch (error) {
-    return Response.json(error);
+    if (error instanceof HttpError) {
+      return Response.json(error.errors, {
+        status: error.status,
+      });
+    }
+    return Response.json(
+      {
+        message: "Somethings happened",
+      },
+      { status: StatusCodes.INTERNAL_SERVER_ERROR }
+    );
   }
 }
